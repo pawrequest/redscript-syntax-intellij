2e16e4890ee1bf60510dbccf7b67ff4b22678757
GitHub Action <action@github.com>
2025-02-24 21:52:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/pawrequest/redscriptsyntaxintellij/MyBundle.kt
+++ src/main/kotlin/com/github/pawrequest/redscriptsyntaxintellij/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pawrequest.redscriptsyntaxintellij
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/pawrequest/redscriptsyntaxintellij/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/pawrequest/redscriptsyntaxintellij/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pawrequest.redscriptsyntaxintellij.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
